193b4186a536691b3fd37775c486655063cb7fed
Natanael Arndt <arndtn@gmail.com>
2016-07-25 17:17:40 +0200

    First version of my Arrays Functions

---
+++ src/Functions.php
@@ -0,0 +1,75 @@
+<?php
+namespace WhiteGecko\Arrays;
+
+/**
+ * This method is from https://secure.php.net/manual/en/function.array-diff.php#91756
+ * by firegun at terra dot com dot br
+ */
+function arrayRecursiveEqual($aArray1, $aArray2)
+{
+    if (isAssoc($aArray1) != isAssoc($aArray2)) {
+        return false;
+    } elseif (isAssoc($aArray1)) {
+        foreach ($aArray1 as $mKey => $mValue) {
+            if (array_key_exists($mKey, $aArray2)) {
+                if (is_array($mValue)) {
+                    $aRecursiveDiff = arrayRecursiveEqual($mValue, $aArray2[$mKey]);
+                    if (!$aRecursiveDiff) {
+                        return false;
+                    }
+                } else {
+                    if ($mValue != $aArray2[$mKey]) {
+                        return false;
+                    }
+                }
+            } else {
+                return false;
+            }
+        }
+    } else {
+        foreach ($aArray1 as $mKey => $mValue) {
+            if (!is_array($mValue)) {
+                $pos = array_search($mValue, $aArray2);
+                if (false === $pos) {
+                    return false;
+                }
+                unset($aArray1[$mKey]);
+                unset($aArray2[$pos]);
+            }
+        }
+        foreach ($aArray1 as $mKey => $mValue) {
+            if (is_array($mValue)) {
+                $found = false;
+                foreach ($aArray2 as $pos => $otherValue) {
+                    if (is_array($otherValue)) {
+                        $aRecursiveDiff = arrayRecursiveEqual($mValue, $otherValue);
+                        if ($aRecursiveDiff) {
+                            $found = true;
+                            break;
+                        }
+                    }
+                }
+                if (!$found) {
+                    return false;
+                }
+                unset($aArray1[$mKey]);
+                unset($aArray2[$pos]);
+            }
+        }
+        if (!empty($aArray1) && !empty($aArray2)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+/**
+ * This is ment to determine between the list of objects and the other hierarchical array structure
+ * objects list, vs subject, predicate and structure in the object
+ * This method is from http://stackoverflow.com/a/173479/414075 by Mark Amery
+ */
+function isAssoc($arr)
+{
+    return array_keys($arr) !== range(0, count($arr) - 1);
+}
---
+++ tests/ArrayEqualTest.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace WhiteGecko\Tests\Arrays;
+
+use PHPUnit\Framework\TestCase;
+use WhiteGecko\Arrays;
+
+class ArrayEqualTest extends TestCase
+{
+    public function testEqualEmpty()
+    {
+        $arrayA = array();
+        $arrayB = array();
+        $this->assertTrue(Arrays\arrayRecursiveEqual($arrayA, $arrayB));
+    }
+
+    public function testEqualSequential()
+    {
+        $arrayA = array('a', 'b', 'c', 'hello', 'z', 'y', 'x');
+        $arrayB = array('z', 'hello', 'y', 'a', 'x', 'c', 'b');
+        $this->assertTrue(Arrays\arrayRecursiveEqual($arrayA, $arrayA));
+        $this->assertTrue(Arrays\arrayRecursiveEqual($arrayA, $arrayB));
+    }
+
+    public function testEqualAssoc()
+    {
+        $arrayA = array('a' => 'a', 'm' => 'b', 'x' => 'c', 1 => 'hello', 5 => 'z', '7' => 'y', 'hello' => 'x');
+        $arrayB = array(5 => 'z', 1 => 'hello', '7' => 'y', 'a' => 'a', 'hello' => 'x', 'x' => 'c', 'm' => 'b');
+        $this->assertTrue(Arrays\arrayRecursiveEqual($arrayA, $arrayA));
+        $this->assertTrue(Arrays\arrayRecursiveEqual($arrayA, $arrayB));
+    }
+
+    public function testEqualHybrid()
+    {
+        $arrayA = array('a' => 'a', 'm' => 'b', 'x' => 'c', 'hello', 'z', 'y', 'x');
+        $arrayB = array('z', 'hello', 'y', 'a' => 'a', 'x', 'x' => 'c', 'm' => 'b');
+        $this->assertTrue(Arrays\arrayRecursiveEqual($arrayA, $arrayA));
+        $this->markTestIncomplete("There is no solution yet for comparing the associative values separate");
+        $this->assertTrue(Arrays\arrayRecursiveEqual($arrayA, $arrayB));
+    }
+
+    public function testEqualRecursiveAssocSequence()
+    {
+        $arrayA = array(
+            'a' => array('a', 'b', 'z', 'x'),
+            'x' => 'c'
+        );
+        $arrayB = array(
+            'x' => 'c',
+            'a' => array('a', 'x', 'z', 'b')
+        );
+        $this->assertTrue(Arrays\arrayRecursiveEqual($arrayA, $arrayA));
+        $this->assertTrue(Arrays\arrayRecursiveEqual($arrayA, $arrayB));
+    }
+
+    public function testEqualRecursiveSequenceAssoc()
+    {
+        $arrayA = array(
+            array('a' => 'a', 'm' => 'b', 'x' => 'c', 1 => 'hello', 5 => 'z', '7' => 'y', 'hello' => 'x'),
+            'a'
+        );
+        $arrayB = array(
+            'a',
+            array(5 => 'z', 1 => 'hello', '7' => 'y', 'a' => 'a', 'hello' => 'x', 'x' => 'c', 'm' => 'b')
+        );
+        $this->assertTrue(Arrays\arrayRecursiveEqual($arrayA, $arrayA));
+        $this->assertTrue(Arrays\arrayRecursiveEqual($arrayA, $arrayB));
+    }
+
+    public function testEqualRecursiveSequenceOfSeqArrays()
+    {
+        $arrayA = array(
+            array('a', 'b', 'z', 'x'),
+            array('a', 'x'),
+            array('y', 'z')
+        );
+        $arrayB = array(
+            array('a', 'x'),
+            array('y', 'z'),
+            array('a', 'x', 'z', 'b')
+        );
+        $this->assertTrue(Arrays\arrayRecursiveEqual($arrayA, $arrayA));
+        $this->assertTrue(Arrays\arrayRecursiveEqual($arrayA, $arrayB));
+    }
+
+    public function testEqualRecursiveSequenceOfAssocArrays()
+    {
+        $arrayA = array(
+            array('a' => 'a', 'm' => 'b', 'x' => 'c', 1 => 'hello', 5 => 'z', '7' => 'y', 'hello' => 'x'),
+            array('a' => 'x'),
+            array('y' => 'z')
+        );
+        $arrayB = array(
+            array('a' => 'x'),
+            array('y' => 'z'),
+            array(5 => 'z', 1 => 'hello', '7' => 'y', 'a' => 'a', 'hello' => 'x', 'x' => 'c', 'm' => 'b')
+        );
+        $this->assertTrue(Arrays\arrayRecursiveEqual($arrayA, $arrayA));
+        $this->assertTrue(Arrays\arrayRecursiveEqual($arrayA, $arrayB));
+    }
+
+    public function testEqualRecursiveSequenceOfArrays()
+    {
+        $arrayA = array(
+            array('a' => 'a', 'm' => 'b', 'x' => 'c', 1 => 'hello', 5 => 'z', '7' => 'y', 'hello' => 'x'),
+            array('a'),
+            array('x', 'y', 'z'),
+            array()
+        );
+        $arrayB = array(
+            array('a'),
+            array(),
+            array(5 => 'z', 1 => 'hello', '7' => 'y', 'a' => 'a', 'hello' => 'x', 'x' => 'c', 'm' => 'b'),
+            array('x', 'z', 'y')
+        );
+        $this->assertTrue(Arrays\arrayRecursiveEqual($arrayA, $arrayA));
+        $this->assertTrue(Arrays\arrayRecursiveEqual($arrayA, $arrayB));
+    }
+
+    public function testEqualHierarchical()
+    {
+        $arrayA = array(
+            'a' => array('a', 'b', 'z', 'x'),
+            'm' => array(
+                'a' => 'x',
+                'z' => array('a', 'c'),
+                'x' => array(array('a' => 'b'), array('c' => 'x'))
+            ),
+            'x' => 'c'
+        );
+        $arrayB = array(
+            'm' => array(
+                'a' => 'x',
+                'x' => array(array('c' => 'x'), array('a' => 'b')),
+                'z' => array('a', 'c')
+            ),
+            'a' => array('a', 'b', 'z', 'x'),
+            'x' => 'c'
+        );
+        $this->assertTrue(Arrays\arrayRecursiveEqual($arrayA, $arrayA));
+        $this->assertTrue(Arrays\arrayRecursiveEqual($arrayA, $arrayB));
+    }
+}
